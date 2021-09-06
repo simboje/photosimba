0c1024ba956c32a19f9e259a00ed76247f27cf61
Siniša Milojević <milojevics@hotmail.com>
2021-09-06 19:51:19 +0200

    Set system look and feel

--- src/main/java/slike/Main.java
+++ src/main/java/slike/Main.java
@@ -3,10 +3,28 @@ package slike;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+        try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InstantiationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
 		JFrame frame = new JFrame("SLIKE");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
