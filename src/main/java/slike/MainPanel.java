ebf5b646c90c9cb483adf5345dd9154969bfb17d
Sinisa Milojevic <sinisa.milojevic@tttech-auto.com>
2021-09-06 19:18:30 +0200

    Add button and labels to GUI
    Button for open function
    Labels for file and test info

--- src/main/java/slike/MainPanel.java
+++ src/main/java/slike/MainPanel.java
@@ -1,10 +1,23 @@
 package slike;
 
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Label;
+
 import javax.swing.JPanel;
 
 public class MainPanel extends JPanel {
 
+	Button openButton = new Button("Open file or directory");
+	Label fileLabel = new Label("File name");
+	Label testLabel = new Label("Test info");
+
 	public MainPanel(String[] args) {
+
+		this.setLayout(new FlowLayout(FlowLayout.LEFT));
+		this.add(openButton);
+		this.add(fileLabel);
+		this.add(testLabel);
 
 	}
 
