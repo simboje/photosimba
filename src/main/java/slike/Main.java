922a3209175af705517fb36d9f856c9a546fe4a0
Siniša Milojević <milojevics@hotmail.com>
2021-09-09 01:06:56 +0200

    Add separate panel for button controls
    Main.java
    * squash UIManager exceptions to one generic
    * add new panel for controls on top of frame (PAGE_START)
    
    OpenButton.java
    * move file chooser code from ImagePanel here
    * invoke ImagePanel to start loading files
    
    ImagePanel.java
    * re-organized, moved code to OpenButton
    * thread start and first image display moved to separate method
    * new method loadFiles is called from OpenButton

--- src/main/java/slike/Main.java
+++ src/main/java/slike/Main.java
@@ -1,11 +1,13 @@
 package slike;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
@@ -13,42 +15,36 @@ public class Main {
 
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (InstantiationException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IllegalAccessException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 		JFrame frame = new JFrame("SLIKE");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		MainPanel panel = new MainPanel(args);
-		frame.add(panel, BorderLayout.CENTER);
-		frame.pack();
+		JPanel topPanel = new JPanel();
+		topPanel.setBackground(Color.WHITE);
+
+		ImagePanel imagePanel = new ImagePanel(args);
+		imagePanel.setBackground(Color.WHITE);
+		imagePanel.setFocusable(true);
+		imagePanel.requestFocusInWindow();
+
+		OpenButton openButton = new OpenButton("Open file or directory", imagePanel);
+
+		topPanel.add(openButton, FlowLayout.LEFT);
+		frame.add(topPanel, BorderLayout.PAGE_START);
+		frame.add(imagePanel, BorderLayout.CENTER);
+
+		frame.setVisible(true);
+		frame.setMinimumSize(new Dimension(800, 600));
+		frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 
 		frame.addWindowListener(new java.awt.event.WindowAdapter() {
 			@Override
 			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-				panel.shutdownThread();
+				imagePanel.shutdownThread();
 			}
 		});
-
-		panel.setFocusable(true);
-		panel.requestFocusInWindow();
-
-		frame.setMinimumSize(new Dimension(800, 600));
-
-		frame.setVisible(true);
-		frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-
 	}
-
 }
