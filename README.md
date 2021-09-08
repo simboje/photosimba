089f4d6bf0020bc496237f41fcc891980b8fc695
Sinisa Milojevic <sinisa.milojevic@tttech-auto.com>
2021-09-08 13:15:19 +0200

    Add support for image and file copy to clipboard
    Implement ClipboardManager, accepts images and files
    Copy can be invoked using Ctrl+S or Ctrl+Shift+S
    
    README.md update - add keyboard controls section
    
    Issues (not user observable)
    getTransferData will be invoked multiple times even in case when
    there is only one copy key combo press (ctrl+s ot ctrl+shift+s).
    
    And in the end it will contain a flavor that is not supported.
    By returning null in this case it is 'pacified' and does not
    cause any issues (for now), but this behaviour needs further
    examination.

--- README.md
+++ README.md
@@ -11,5 +11,14 @@ Intention is to create something to be used instead of default Photos applicatio
 ## How to build and use
 A standard Maven project, build from cmd or use Eclipse. Currently buildable as jar but [launch4j](http://launch4j.sourceforge.net/docs.html) config will be added later in order to get .exe for Windows users.
 
+## Keyboard Controls
+* A or Left arrow - previous image
+* D or Right Arrow - next image
+* W or Up arrow - rotate image counter-clockwise (does not save changes to image file)
+* S or Down arrow - rotate image clockwise (does not save changes to image file)
+* Ctrl + C - copies image to clipboard (can be pasted to other image programs)
+* Ctrl + Shift + C - copies image file to clipboard (can be pasted in file explorer)
+
 ## Credits
-Image drag and zoom code taken from [stackoverflow](https://stackoverflow.com/questions/58021358/java-how-to-do-image-manipulation-pan-zoom-flip-and-rotate).
+Image drag and zoom code taken from [this example](https://stackoverflow.com/questions/58021358/java-how-to-do-image-manipulation-pan-zoom-flip-and-rotate).
+Copy image to clipboard [example](https://stackoverflow.com/questions/4552045/copy-bufferedimage-to-clipboard) , copy file to clipboard [example](https://stackoverflow.com/questions/31798646/can-java-system-clipboard-copy-a-file)
---
+++ src/main/java/slike/ClipboardManager.java
@@ -0,0 +1,64 @@
+package slike;
+
+import java.awt.*;
+import java.awt.datatransfer.*;
+import java.io.*;
+
+public class ClipboardManager implements ClipboardOwner, Transferable {
+
+	Image image;
+	java.util.List<File> listOfFiles;
+
+	public void copyImage() {
+		Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+		c.setContents(this, this);
+	}
+
+	public void copyFile() {
+		Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+		c.setContents(this, this);
+	}
+
+	public void lostOwnership(Clipboard clip, Transferable trans) {
+		System.out.println("Lost Clipboard Ownership");
+	}
+
+	public ClipboardManager(Image img) {
+		this.image = img;
+	}
+
+	public ClipboardManager(java.util.List<File> listOfFiles) {
+		this.listOfFiles = listOfFiles;
+	}
+
+	@Override
+	public Object getTransferData(DataFlavor flavor) {
+		if (flavor.equals(DataFlavor.imageFlavor) && image != null) {
+			return image;
+		} else if (flavor.equals(DataFlavor.javaFileListFlavor) && listOfFiles != null) {
+			return listOfFiles;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public DataFlavor[] getTransferDataFlavors() {
+		DataFlavor[] flavors = new DataFlavor[2];
+		flavors[0] = DataFlavor.imageFlavor;
+		flavors[1] = DataFlavor.javaFileListFlavor;
+		return flavors;
+	}
+
+	@Override
+	public boolean isDataFlavorSupported(DataFlavor flavor) {
+		DataFlavor[] flavors = getTransferDataFlavors();
+		for (int i = 0; i < flavors.length; i++) {
+			if (flavor.equals(flavors[i])) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
