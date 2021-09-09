a1b2ca6fd9662df20c3a0a33ac727b1abb2112b7
Siniša Milojević <milojevics@hotmail.com>
2021-09-09 16:49:51 +0200

    Add icon
    JFrame works with .png
    launch4j only with .ico, crashes with png

--- src/main/java/slike/Main.java
+++ src/main/java/slike/Main.java
@@ -4,7 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.io.File;
+import java.nio.file.Files;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,6 +25,8 @@ public class Main {
 
 		JFrame frame = new JFrame("SLIKE");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		ImageIcon icon = new ImageIcon("program_icon.png");
 
 		JPanel topPanel = new JPanel();
 		topPanel.setBackground(Color.WHITE);
@@ -41,6 +46,7 @@ public class Main {
 		frame.add(imagePanel, BorderLayout.CENTER);
 
 		frame.setVisible(true);
+		frame.setIconImage(icon.getImage());
 		frame.setMinimumSize(new Dimension(800, 600));
 		frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 
