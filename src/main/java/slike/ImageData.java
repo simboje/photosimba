2b9125d3fcaa05d376bf47cf6b7d9f40108e0950
Sinisa Milojevic <sinisa.milojevic@tttech-auto.com>
2021-09-11 22:40:56 +0200

    Read and store exif image data
    Image data is now accompanied by rotation data
    Rotation data is used to properly orientate the image
    
    ImageLoaderThread.java
    Remove code that was used for sub-optimal image rotate routine

---
+++ src/main/java/slike/ImageData.java
@@ -0,0 +1,27 @@
+package slike;
+
+import java.awt.image.BufferedImage;
+
+public class ImageData  {
+	
+	BufferedImage image;
+	int rotation = 0;
+	
+	public ImageData(BufferedImage image)
+	{
+		this.image = image;
+	}
+
+	public void setRotation(int rotation) {
+		this.rotation = rotation;	
+	}
+
+	public int getRotation() {
+		return rotation;
+	}
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+}
